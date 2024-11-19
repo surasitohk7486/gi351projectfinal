9b99818aaa55ba1d58e7644031f2e7f25a0fd2b6
Nangil <surasit.dang@bumail.net>
2024-11-19 19:40:21 +0700

    Ladder

---
+++ Assets/Scripts/Ladder.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Ladder : MonoBehaviour
+{
+
+    public Transform playerController;
+    bool inside = false;
+    public float speed = 3f;
+    public FirstPersonController player;
+    //public AudioSource sound;
+
+    void Start()
+    {
+        player = GetComponent<FirstPersonController>();
+        inside = false;
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.gameObject.tag == "Ladder")
+        {
+            Debug.Log("TouchingLadderTrue");
+            //player.enabled = false;
+            inside = !inside;
+        }
+
+
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+
+        if (col.gameObject.tag == "Ladder")
+        {
+            Debug.Log("TouchingLadderFalse");
+            //player.enabled = true;
+            inside = !inside;
+        }
+
+
+    }
+
+    void Update()
+    {
+        if (inside == true && Input.GetKey(KeyCode.W))
+        {
+            player.transform.position += Vector3.up /
+            speed * Time.deltaTime;
+        }
+        else if (inside == true && Input.GetKey(KeyCode.S))
+        {
+            player.transform.position += Vector3.down /
+            speed * Time.deltaTime;
+        }
+
+        /*if (inside == true && Input.GetKey(KeyCode.W))
+        {
+            sound.enabled = true;
+            sound.loop = true;
+        }
+        else
+        {
+            sound.enabled = false;
+            sound.loop = false;
+        }*/
+    }
+
+
+}
