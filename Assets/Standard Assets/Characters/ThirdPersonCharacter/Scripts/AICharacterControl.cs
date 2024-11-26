e143be2cec737e0c7dd426c2eb95754d6206bd2e
Nangil <surasit.dang@bumail.net>
2024-11-26 21:29:58 +0700

    Susu

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
-    [RequireComponent(typeof (NavMeshAgent))]
+    //[RequireComponent(typeof (NavMeshAgent))]
     [RequireComponent(typeof (ThirdPersonCharacter))]
     public class AICharacterControl : MonoBehaviour
     {
-        public NavMeshAgent agent { get; private set; } // the navmesh agent required for the path finding
+        //public NavMeshAgent agent { get; private set; } // the navmesh agent required for the path finding
         public ThirdPersonCharacter character { get; private set; } // the character we are controlling
         public Transform target; // target to aim for
 
@@ -15,11 +15,11 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private void Start()
         {
             // get the components on the object we need ( should not be null due to require component so no need to check )
-            agent = GetComponentInChildren<NavMeshAgent>();
+            //agent = GetComponentInChildren<NavMeshAgent>();
             character = GetComponent<ThirdPersonCharacter>();
 
-	        agent.updateRotation = false;
-	        agent.updatePosition = true;
+	        //agent.updateRotation = false;
+	        //agent.updatePosition = true;
         }
 
 
@@ -28,12 +28,12 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         {
             if (target != null)
             {
-                agent.SetDestination(target.position);
+                //agent.SetDestination(target.position);
 
 				
 				
                 // use the values to move the character
-                character.Move(agent.desiredVelocity, false, false);
+                //character.Move(agent.desiredVelocity, false, false);
             }
             else
             {
