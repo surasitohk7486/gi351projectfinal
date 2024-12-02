55ec655479c58897ba5341ddd15875cc003628cb
Nangil <surasit.dang@bumail.net>
2024-12-02 09:45:52 +0700

    Finish???

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI; // อ้างอิง UI Panel
+
+    private bool isPaused = false;
+
+    
+
+    void Update()
+    {
+        // ตรวจสอบการกดปุ่ม Esc
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGameEsc();
+            }
+        }
+    }
+
+    public void PauseGameEsc()
+    {
+        // แสดง UI และหยุดเกม
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; // หยุดเวลาในเกม
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        // ซ่อน UI และเริ่มเกมต่อ
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; // เริ่มเวลาในเกมใหม่
+        isPaused = false;
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        // รีเซ็ตเวลาและโหลด Scene ใหม่
+        Time.timeScale = 1f; // เริ่มเวลาใหม่ก่อนเปลี่ยน Scene
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/Scripts/ShowCursorMouse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCursorMouse : MonoBehaviour
+{
+    void Start()
+    {
+        // แสดง Cursor
+        Cursor.visible = true;
+
+        // ปลดล็อก Cursor จากหน้าจอ
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    void OnDisable()
+    {
+        // ซ่อน Cursor
+        Cursor.visible = false;
+
+        // ล็อก Cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
